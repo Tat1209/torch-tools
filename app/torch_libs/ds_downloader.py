fcf7ecf592deff2ffb0a502444ccd4ea6e12f907
Tat1209 <taikunn12740pqp@icloud.com>
2024-11-11 14:22:39 +0900

    sync: hig63

--- app/torch_libs/ds_downloader.py
+++ app/torch_libs/ds_downloader.py
@@ -13,5 +13,6 @@ sys.path.append(torchlib_path)
 root = f"{work_path}/assets/datasets/"
 
 # torchvision.datasets.CIFAR10(root=root, train=True, download=True)
-torchvision.datasets.Flowers102(root=root, download=True)
+torchvision.datasets.STL10(root=root, download=True)
+# torchvision.datasets.Flowers102(root=root, download=True)
 
